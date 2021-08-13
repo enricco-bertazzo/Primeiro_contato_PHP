9442974a1c31033a8fbfc7e4a6bdeac8d7d642b7
Enricco Bertazzo <enriccowhz@gmail.com>
2021-08-12 21:06:09 -0300

    começando

---
+++ Primeiro_exercicio.php
@@ -0,0 +1,13 @@
+<?php
+    $num = 85
+    $numsoma = 0
+    while($num <= 901){
+        if($num % 2 ==0){
+            print("$num")
+            $numsoma += $num
+        }
+    $num += 1
+    }
+print("Soma de todos os pares é igual: $numsoma")
+
+?>
